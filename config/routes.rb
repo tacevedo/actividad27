00dae2a760f6fde0cb967114b27bc3dc393aa2af
tam <tamara.acevedoavila@gmail.com>
2018-06-20 20:17:06 -0400

    inicio

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  resources :categories do
+    resources :products, only: [:create]
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'categories#index'
+end
