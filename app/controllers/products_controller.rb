1ab9c7c8e353721856fd8376e91c88d6a33d5892
tam <tamara.acevedoavila@gmail.com>
2018-06-20 23:14:02 -0400

    algo

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,17 @@
 class ProductsController < ApplicationController
+  # before_action :find_todo, only: [:show, :edit, :update, :destroy, :complete]
+
+  def create
+    @product = Product.new(product_params)
+    @product.save!
+    redirect_to categories_path
+  end
+
+
+
+  private
+
+    def product_params
+      params.require(:product).permit(:name, :price)
+    end
 end
