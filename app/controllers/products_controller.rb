eef8627a5b05ad3d4f7cee548f98f748573cf585
tam <tamara.acevedoavila@gmail.com>
2018-06-21 23:19:17 -0400

    mejoras

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,16 +1,38 @@
 class ProductsController < ApplicationController
-  # before_action :find_todo, only: [:show, :edit, :update, :destroy, :complete]
+  before_action :find_product, only: [:destroy]
 
   def create
+    # @product = Product.new(product_params)
+    # @product.save!
+    # redirect_to @product.category
+
+
     @product = Product.new(product_params)
-    @product.save!
-    redirect_to categories_path
+    @product.category_id = params[:category_id]
+    respond_to do |format|
+
+      if @product.save
+        format.html { redirect_to @product.category, notice: 'Product was successfully created.' }
+        format.json { render :show, status: :created, location: @product }
+      else
+        format.html { render :new }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
-
+  def destroy
+    @product.destroy
+    respond_to do |format|
+      format.html { redirect_to @product.category, notice: 'Product was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
 
   private
-
+    def find_product
+      @product = Product.find(params[:id])
+    end
     def product_params
       params.require(:product).permit(:name, :price)
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :categories do
-    resources :products, only: [:create]
+    resources :products, only: [:create, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'categories#index'
